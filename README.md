cb5375cb88748a5f1ab52bb3fe5a061c413bc81e
elijahwale <walexy730@gmail.com>
2020-03-22 19:09:22 +0100

    task done in javascript

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# javascript-task
+<h1 style="color:red;">Javascript Task</h1>
+<p>store your name in a variable</p>
+<p>store your courses in an array</p>
+<p>Display your name and your courses</p>
+<p>if the number of Courses  you are doing is an even number, find and display all even numbers from 1-200</p>
---
+++ script.js
@@ -0,0 +1,26 @@
+// store your name in a variable
+var name = "Olawale Esan";
+
+
+//store your courses in an array
+
+var courses = ["html", "css", "javascript", "php"];
+
+// Display your name 
+
+console.log(name);
+
+// Display your courses
+
+console.log(courses);
+
+
+
+
+// if the number of Courses  you are doing is an even number, find and display all even numbers
+// from 1-200
+for (var i=1; i<=200; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
